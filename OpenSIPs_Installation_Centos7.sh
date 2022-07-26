e25e99ede3ea35e54d13e5e53754eb5ff0a614de
shadikur <me@shadikur.com>
2022-07-27 00:34:51 +0200

    PHP7.3 and Apache

--- OpenSIPs_Installation_Centos7.sh
+++ OpenSIPs_Installation_Centos7.sh
@@ -85,26 +85,74 @@ sleep 3
 warning "Starting installation..."
 yum install https://yum.opensips.org/3.2/releases/el/7/x86_64/opensips-yum-releases-3.2-6.el7.noarch.rpm -y
 yum install opensips opensips-cli -y
-syytemctl enable opensips.service
+systemctl enable opensips.service
 systemctl start opensips.service
 
 #Install OpenSIPs RTPProxy Engine
 verbose "Installing OpenSIPs RTPProxy Engine"
-sleep 3
-yum install opensips-rtpproxy -y
-systemctl enable opensips-rtpproxy.service
-systemctl start opensips-rtpproxy.service
+sleep 2
+useradd rtpproxy
+cd /usr/src
+git clone -b master https://github.com/sippy/rtpproxy.git
+git -C rtpproxy submodule update --init --recursive
+cd rtpproxy
+./configure
+make clean all
+make install
+
+warning "Adding OpenSIP RTPProxy as a service"
+cat <<EOF > /lib/systemd/system/rtpproxy.service
+[Unit]
+Description=RTPProxy media server
+After=network.target
+Requires=network.target
+[Service]
+Type=simple
+PIDFile=/var/run/rtpproxy/rtpproxy.pid
+Environment='OPTIONS= -f -L 4096 -l 0.0.0.0 -m 10000 -M 20000 -d INFO:LOG_LOCAL5'
+Restart=always
+RestartSec=5
+ExecStartPre=-/bin/mkdir /var/run/rtpproxy
+ExecStartPre=-/bin/chown rtpproxy:rtpproxy /var/run/rtpproxy
+ExecStart=/usr/local/bin/rtpproxy -p /var/run/rtpproxy/rtpproxy.pid -s udp:127.0.0.1:22222 \
+ -u rtpproxy:rtpproxy -n udp:127.0.0.1:22223 $OPTIONS
+ExecStop=/usr/bin/pkill -F /var/run/rtpproxy/rtpproxy.pid
+ExecStopPost=-/bin/rm -R /var/run/rtpproxy
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=rtpproxy
+SyslogFacility=local5
+TimeoutStartSec=10
+TimeoutStopSec=10
+[Install]
+WantedBy=multi-user.target
+EOF
+
+chmod +x /lib/systemd/system/rtpproxy.service 
+systemctl enable rtpproxy.service
+systemctl start rtpproxy.service
 
 
 #MySQL Server & PHP Installation 
 verbose "Installing MySQL Server & PHP"
 sleep 3
-#install PHP 7.3
-yum install php73u php73u-mysqlnd php73u-xml php73u-json php73u-mbstring php73u-gd php73u-intl php73u-opcache php73u-devel php73u-pear php73u-pecl-apcu php73u-pecl-imagick php73u-pecl-memcached php73u-pecl-redis php73u-pecl-xdebug php73u-pecl-yaml php73u-pecl-zip php73u-pecl-geoip php73u-pecl-geoip-devel -y
-#install MySQL Server
-yum install mysql-server -y
-systemctl enable mysqld.service
-systemctl start mysqld.service
+#install MariaDB 10.3
+yum install mariadb-server mariadb-devel mariadb-libs mariadb-server-libs -y
+systemctl enable mariadb.service
+systemctl start mariadb.service
+#Add PHP 7.3 Remi repository
+yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
+#Disable 5.4 and enable 7.3
+yum-config-manager --disable remi-php54
+yum-config-manager --enable remi-php73
+#install PHP 7.3 remi repository
+yum install php php-fpm php-mysqlnd php-mbstring php-mcrypt php-xml php-xmlrpc php-soap php-intl php-gd php-json php-opcache php-pdo php-pdo_mysql -y
+
+#install apache2 and enable php7.3
+yum install httpd httpd-devel httpd-tools -y
+systemctl enable httpd.service
+systemctl start httpd.service
+
 
 #Update MySQL password
 verbose "Update MySQL password"
@@ -112,6 +160,7 @@ sleep 3
 MySQLPass=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
 warning "Changing MySQL password to $MySQLPass"
 sleep 2
+warning "Securing mysql sever..."
 mysqladmin -u root password $MySQLPass
 mysql -u root -p$MySQLPass -e "UPDATE mysql.user SET Password=PASSWORD('$MySQLPass') WHERE User='root';"
 mysql -u root -p$MySQLPass -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', 'localhost.localdomain');"
@@ -127,16 +176,16 @@ mysql -u root -p$MySQLPass -e "GRANT ALL PRIVILEGES ON opensips.* TO 'opensips'@
 mysql -u root -p$MySQLPass -e "GRANT ALL PRIVILEGES ON opensips.* TO 'opensips'@'%' IDENTIFIED BY '$MySQLPass';"
 mysql -u root -p$MySQLPass -e "FLUSH PRIVILEGES;"
 
-warning "Securing mysql sever..."
 sleep 2
 
 error "MySQL ROOT Password set to $MySQLPass"
 
 #Update PHP Settings
+verbose "Update PHP Settings"
 sleep 2
-sed 's#post_max_size = .*#post_max_size = 80M#g' -i /etc/php/7.3/apache2/php.ini
-sed 's#upload_max_filesize = .*#upload_max_filesize = 80M#g' -i /etc/php/7.3/apache2/php.ini
-sed 's#;max_input_vars = .*#max_input_vars = 8000#g' -i /etc/php/7.3/apache2/php.ini
+sed 's#post_max_size = .*#post_max_size = 80M#g' -i /etc/php.ini
+sed 's#upload_max_filesize = .*#upload_max_filesize = 80M#g' -i /etc/php.ini
+sed 's#;max_input_vars = .*#max_input_vars = 8000#g' -i /etc/php.ini
 
 
 verbose "Cloning OpenSIPs GUI interface"
@@ -147,25 +196,25 @@ cp config/tools/system/smonitor/opensips_stats_cron /etc/cron.d/
 systemctl restart cron
 
 #OpenSIPs CLI configuration
-OpenSIPDBPass=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
+verbose "Configuring OpenSIPs CLI"
 cat << EOF > /etc/opensips/opensips-cli.cfg
 [default]
 database_name=opensips
-database_url=mysql://opensips:$OpenSIPDBPass@localhost
-template_uri=mysql://opensips:$OpenSIPDBPass@localhost
-database_admin_url=mysql://root:$DBPASS@localhost
+database_url=mysql://opensips:$MySQLPass@localhost
+template_uri=mysql://opensips:$MySQLPass@localhost
+database_admin_url=mysql://root:$MySQLPass@localhost
 database_modules=ALL
 database_force_drop=true
 EOF
 opensips-cli -x database create
 mysql -p$DBPASS -Dopensips < config/db_schema.mysql
-sed -i "s/$config->db_pass = .*/$config->db_pass = \"$OpenSIPDBPass\";/g" /var/www/html/opensips-cp/config/db.inc.php
+sed -i "s/$config->db_pass = .*/$config->db_pass = \"$MySQLPass\";/g" /var/www/html/opensips-cp/config/db.inc.php
 verbose "Installation has been completed"
 ip=$(hostname -I | awk '{print $1}')
 verbose "Please visit http://$ip/opensips-cp/web/ and start configuring your opensips"
 warning "Default username: admin & password: opensips, Please change it immediately.\n\n"
 verbose "Please keep this information in your record for further configuration"
-verbose "MySQL ROOT Password: $DBPASS"
-verbose	"OpenSIP DB Name: opensips, DB Username: opensips & DB Password: $OpenSIPDBPass"
+verbose "MySQL ROOT Password: $MySQLPass"
+verbose	"OpenSIP DB Name: opensips, DB Username: opensips & DB Password: $MySQLPass"
 verbose "Monit Username: admin and Password: $MonitPass\n\n"
 error "If you face any issue on deployment or in configuration, please feel free to contact to jbansal@guidepoint.com or mrahman@guidepoint.com. Thank you.\n\n"
