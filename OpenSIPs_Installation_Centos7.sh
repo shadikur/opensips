318c3f3a191a6c5c42c4af4ec8d54224b96806a0
shadikur <me@shadikur.com>
2022-07-28 11:13:04 +0200

    MySQL update

--- OpenSIPs_Installation_Centos7.sh
+++ OpenSIPs_Installation_Centos7.sh
@@ -182,23 +182,36 @@ yum install httpd httpd-devel httpd-tools -y
 systemctl enable httpd.service
 systemctl start httpd.service
 
-
-#Update MySQL password
 verbose "Update MySQL password"
-sleep 3
-MySQLPass=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
-warning "Changing MySQL password to $MySQLPass"
-sleep 2
-warning "Securing mysql sever..."
-mysqladmin -u root password $MySQLPass
-mysql -u root -p$MySQLPass -e "UPDATE mysql.user SET Password=PASSWORD('$MySQLPass') WHERE User='root';"
-mysql -u root -p$MySQLPass -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', 'localhost.localdomain');"
-mysql -u root -p$MySQLPass -e "DELETE FROM mysql.user WHERE User='';"
-mysql -u root -p$MySQLPass -e "FLUSH PRIVILEGES;"
+DBPASS=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
+warning "Changing MySQL password to $DBPASS"
+SECURE_MYSQL=$(expect -c "
+set timeout 10
+spawn mysql_secure_installation
+expect \"Enter current password for root (enter for none):\"
+send \"$MYSQL\r\"
+expect \"Change the root password?\"
+send \"y\r\"
+expect \"New password:\"
+send  \"$DBPASS\"
+expect \"Re-enter new password:\"
+send \"$DBPASS\"
+expect \"Remove anonymous users?\"
+send \"y\r\"
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+expect eof
+")
+systemctl restart mysql
+echo "$SECURE_MYSQL"
+
+error "MySQL ROOT Password set to $DBPASS"
 
 sleep 2
-
-error "MySQL ROOT Password set to $MySQLPass"
 
 #Update PHP Settings
 verbose "Update PHP Settings"
@@ -227,9 +240,9 @@ verbose "Configuring OpenSIPs CLI"
 cat << EOF > /etc/opensips/opensips-cli.cfg
 [default]
 database_name=opensips
-database_url=mysql://opensips:$MySQLPass@localhost
-template_uri=mysql://opensips:$MySQLPass@localhost
-database_admin_url=mysql://root:$MySQLPass@localhost
+database_url=mysql://opensips:$DBPASS@localhost
+template_uri=mysql://opensips:$DBPASS@localhost
+database_admin_url=mysql://root:$DBPASS@localhost
 database_modules=ALL
 database_force_drop=true
 EOF
@@ -251,14 +264,14 @@ touch ~/.opensips-cli.cfg
 
 opensips-cli -x database create
 #import database schema
-mysql -u root -p$MySQLPass -Dopensips < /var/www/html/opensips-cp/config/db_schema.mysql
-sed -i "s/$config->db_pass = .*/$config->db_pass = \"$MySQLPass\";/g" /var/www/html/opensips-cp/config/db.inc.php
+mysql -u root -p$DBPASS -Dopensips < /var/www/html/opensips-cp/config/db_schema.mysql
+sed -i "s/$config->db_pass = .*/$config->db_pass = \"$DBPASS\";/g" /var/www/html/opensips-cp/config/db.inc.php
 verbose "Installation has been completed"
 ip=$(hostname -I | awk '{print $1}')
 verbose "Please visit http://$ip/opensips-cp/web/ and start configuring your opensips"
 warning "Default username: admin & password: opensips, Please change it immediately."
 verbose "Please keep this information in your record for further configuration"
-verbose "MySQL ROOT Password: $MySQLPass"
-verbose	"OpenSIP DB Name: opensips, DB Username: opensips & DB Password: $MySQLPass"
+verbose "MySQL ROOT Password: $DBPASS"
+verbose	"OpenSIP DB Name: opensips, DB Username: opensips & DB Password: $DBPASS"
 verbose "Monit Username: admin and Password: $MonitPass"
 error "If you face any issue on deployment or in configuration, please feel free to contact to jbansal@guidepoint.com or mrahman@guidepoint.com. Thank you."
